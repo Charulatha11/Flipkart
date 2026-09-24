9e151a1825a66bc6cfa95580f2d4be8965aee279
Charulatha11 <charukalai11@gmail.com>
2026-09-24 15:04:38 +0530

    ModifiedMouse

--- tests/Mouse.spec.js
+++ tests/Mouse.spec.js
@@ -1,7 +1,7 @@
 import {test} from '@playwright/test';
 test('Mouse Actions', async ({ page }) => {
         await page.goto('https://demoqa.com/buttons');
-        await page.locator('#doubleClickBtn').dblclick();
+        
         await page.locator('#rightClickBtn').click({button:'right'});
         await page.locator('//button[text()="Click Me"]').click();
 
